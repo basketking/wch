6ad92020b228dd0405b62f918ba52f37847e876a
basketking <75998108+basketking@users.noreply.github.com>
2023-09-29 14:19:00 +0200

    Add files via upload

--- st.py
+++ st.py
@@ -3,8 +3,6 @@ import numpy as np
 import streamlit as st
 
 df = pd.read_excel('https://github.com/basketking/wch/blob/main/results_individuals.xlsx?raw=true')
-df.head()
-df.columns
 df_group = df.groupby('playerAName').agg({
     'associationPlayer_A': 'count',
     'pointTotal_A': [('sum_pointTotal_A', 'sum'), ('average_pointTotal_A', 'mean')],
@@ -13,7 +11,6 @@ df_group = df.groupby('playerAName').agg({
     'setsWon_A': [('sum_setsWon_X', 'sum'), ('average_setsWon_X', 'mean')],
 })
 # df_group.columns = df_group.columns.get_level_values(0)
-df_group
 
 st.table(df_group)
 
